59abafca0c0f30897646244be4f6216a929c380d
Elchin <mr.chelyabiev14@gmail.com>
2018-09-09 23:06:30 +0300

    Initial

---
+++ Homework3: Elchyn Cheliabiyeu/Event.py
@@ -0,0 +1,24 @@
+import threading
+
+
+def print_number(number, event, next_event):
+    while number <= 100:
+        event.wait()
+        event.clear()
+        print(number)
+        number += 5
+        next_event.set()
+
+
+if __name__ == '__main__':
+    events = [threading.Event() for _ in range(5)]
+    next_events = events[1:] + events[:1]
+    threads = [threading.Thread(target=print_number,
+                                args=(num, event, next_event))
+               for num, (event, next_event)
+               in enumerate(zip(events, next_events))]
+    for thread in threads:
+        thread.start()
+    events[0].set()
+    for thread in threads:
+        thread.join()
---
+++ Homework3: Elchyn Cheliabiyeu/Semaphore.py
@@ -0,0 +1,30 @@
+import threading
+
+
+def print_number(number, mutexes):
+    mutex_pos = number
+    while number <= 100:
+        try:
+            mutexes[mutex_pos].acquire(timeout=1)
+        except ValueError:
+            continue
+        print(number)
+        number += 5
+        if mutex_pos == len(mutexes) - 1:
+            mutexes[0].release()
+        else:
+            mutexes[mutex_pos + 1].release()
+
+
+if __name__ == '__main__':
+    mutexes = [threading.BoundedSemaphore() for _ in range(5)]
+    for mutex in mutexes:
+        mutex.acquire()
+    mutexes[0].release()
+    threads = [threading.Thread(name='Thread {}'.format(num),
+                                target=print_number, args=(num, mutexes))
+               for num in range(5)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
