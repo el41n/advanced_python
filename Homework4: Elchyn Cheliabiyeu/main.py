3f1e5203de63e7bbc7d4dd28d2db99a481f24473
Elchin <mr.chelyabiev14@gmail.com>
2018-09-10 20:38:19 +0300

    Checkstyle fix

--- Homework4: Elchyn Cheliabiyeu/main.py
+++ Homework4: Elchyn Cheliabiyeu/main.py
@@ -12,15 +12,17 @@ def print_num(num, step, syncronize):
         if number == len(syncronize) - 1:
             syncronize[0].release()
         else:
-            syncronize[number+1].release()
+            syncronize[number + 1].release()
 
 
 if __name__ == '__main__':
     syncronize = [multiprocessing.Lock() for _ in range(2)]
     for sync in syncronize:
         sync.acquire()
-    p1 = multiprocessing.Process(name="Process 1", target=print_num, args=(0, 2, syncronize))
-    p2 = multiprocessing.Process(name="Process 2", target=print_num, args=(1, 2, syncronize))
+    p1 = multiprocessing.Process(name="Process 1", target=print_num,
+                                 args=(0, 2, syncronize))
+    p2 = multiprocessing.Process(name="Process 2", target=print_num,
+                                 args=(1, 2, syncronize))
     p1.start()
     p2.start()
     syncronize[0].release()
